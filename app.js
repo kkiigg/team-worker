18d3056fe7351047ca4ed022ff90ce30c9bff5c1
zhaowei <00008888zw@sina.com>
2020-12-27 22:24:29 +0800

    ready to push dev

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.route('*').all(function(req,res,next){
 	}
 })
 
-console.log('i changed from 2 change again')
+console.log('file change 1')
 //DEBUG
 
 //app.params(['id','page'],function(){})
